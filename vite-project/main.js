efe09141ede8c017910c11cbc53c529d1b9548f2
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-18 20:31:16 +0330

    files created

---
+++ vite-project/main.js
@@ -0,0 +1,1 @@
+import './src/Styles/index.css';
---
+++ vite-project/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./main.js', './src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
